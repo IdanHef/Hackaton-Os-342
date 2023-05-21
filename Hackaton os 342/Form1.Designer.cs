4349ab16ca28dc3a8921bca1864cb215637c4a1e
IdanHef <idanhefetz8@gmail.com>
2023-05-21 11:59:36 +0300

    producers consumers screen

--- Hackaton os 342/Form1.Designer.cs
+++ Hackaton os 342/Form1.Designer.cs
@@ -28,25 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.numberOfProducersLabel = new System.Windows.Forms.Label();
+            this.numberOfProducersTextBox = new System.Windows.Forms.TextBox();
+            this.numberOfConsumersLabel = new System.Windows.Forms.Label();
+            this.numberOfConsumersTextBox = new System.Windows.Forms.TextBox();
+            this.producersRateLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // titleLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(333, 68);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(309, 36);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(188, 20);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Producer-Consumer Screen";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numberOfProducersLabel
+            // 
+            this.numberOfProducersLabel.AutoSize = true;
+            this.numberOfProducersLabel.Location = new System.Drawing.Point(145, 121);
+            this.numberOfProducersLabel.Name = "numberOfProducersLabel";
+            this.numberOfProducersLabel.Size = new System.Drawing.Size(153, 20);
+            this.numberOfProducersLabel.TabIndex = 1;
+            this.numberOfProducersLabel.Text = "Number of Producers:";
+            // 
+            // numberOfProducersTextBox
+            // 
+            this.numberOfProducersTextBox.Location = new System.Drawing.Point(441, 118);
+            this.numberOfProducersTextBox.Name = "numberOfProducersTextBox";
+            this.numberOfProducersTextBox.Size = new System.Drawing.Size(125, 27);
+            this.numberOfProducersTextBox.TabIndex = 2;
+            // 
+            // numberOfConsumersLabel
+            // 
+            this.numberOfConsumersLabel.AutoSize = true;
+            this.numberOfConsumersLabel.Location = new System.Drawing.Point(145, 173);
+            this.numberOfConsumersLabel.Name = "numberOfConsumersLabel";
+            this.numberOfConsumersLabel.Size = new System.Drawing.Size(160, 20);
+            this.numberOfConsumersLabel.TabIndex = 3;
+            this.numberOfConsumersLabel.Text = "Number of Consumers:";
+            // 
+            // numberOfConsumersTextBox
+            // 
+            this.numberOfConsumersTextBox.Location = new System.Drawing.Point(441, 173);
+            this.numberOfConsumersTextBox.Name = "numberOfConsumersTextBox";
+            this.numberOfConsumersTextBox.Size = new System.Drawing.Size(125, 27);
+            this.numberOfConsumersTextBox.TabIndex = 4;
+            // 
+            // producersRateLabel
+            // 
+            this.producersRateLabel.AutoSize = true;
+            this.producersRateLabel.Location = new System.Drawing.Point(145, 232);
+            this.producersRateLabel.Name = "producersRateLabel";
+            this.producersRateLabel.Size = new System.Drawing.Size(160, 20);
+            this.producersRateLabel.TabIndex = 5;
+            this.producersRateLabel.Text = "Number of Consumers:";
+            this.producersRateLabel.Click += new System.EventHandler(this.producersRateLabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.producersRateLabel);
+            this.Controls.Add(this.numberOfConsumersTextBox);
+            this.Controls.Add(this.numberOfConsumersLabel);
+            this.Controls.Add(this.numberOfProducersTextBox);
+            this.Controls.Add(this.numberOfProducersLabel);
+            this.Controls.Add(this.titleLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +108,11 @@
 
         #endregion
 
-        protected internal Label label1;
+        protected internal Label titleLabel;
+        private Label numberOfProducersLabel;
+        private TextBox numberOfProducersTextBox;
+        private Label numberOfConsumersLabel;
+        private TextBox numberOfConsumersTextBox;
+        private Label producersRateLabel;
     }
 }
--- Hackaton os 342/Form1.cs
+++ Hackaton os 342/Form1.cs
@@ -46,5 +46,6 @@ namespace Hackaton_os_342
         {
 
         }
+
     }
 }
