ee938b177c63368420db48f3c002591e0c996ef9
IdanHef <idanhefetz8@gmail.com>
2023-05-21 19:27:33 +0300

    buffer

--- Hackaton os 342/Consumer.cs
+++ Hackaton os 342/Consumer.cs
@@ -8,5 +8,41 @@ namespace Hackaton_os_342
 {
     internal class Consumer
     {
+        private readonly Buffer buffer;
+
+        public Consumer(Buffer buffer)
+        {
+            this.buffer = buffer;
+        }
+
+        public void ConsumeItems(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                // Get an occupied chair from the buffer
+                Chair chair = buffer.GetOccupiedChair();
+
+                if (chair != null)
+                {
+                    // Retrieve the item from the chair
+                    string item = chair.Item;
+
+                    // Release the chair and consume the item
+                    chair.ReleaseChair();
+                    chair.ReleaseAnimal();
+                    chair.Item = null;
+
+                    Console.WriteLine($"Consumer consumed {item} from chair {chair.Id}");
+                }
+                else
+                {
+                    Console.WriteLine($"Consumer couldn't find an occupied chair. Waiting...");
+                    // Add a delay or wait here before trying again
+                }
+
+                // Simulate some delay between consuming items
+                Thread.Sleep(1500);
+            }
+        }
     }
 }
